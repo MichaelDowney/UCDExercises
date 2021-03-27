00b2ef0a1ccb4ae5b41b595e2906e12e48e28a35
Michael Downey <michaeldowney99@gmail.com>
2021-03-27 11:59:45 +0000

    Added variable exercises from data camp

---
+++ Module2_Variables.py
@@ -0,0 +1,57 @@
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print (savings)
+
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+result = savings * growth_multiplier ** 7
+
+# Print out result
+print (result)
+
+# int, or integer: a number without a fractional part. savings, with the value 100, is an example of an integer.
+# float, or floating point: a number that has both an integer and fractional part, separated by a point. growth_multiplier, with the value 1.1, is an example of a float.
+# str, or string: a type to represent text. You can use single or double quotes to build a string.
+# bool, or boolean: a type to represent logical values. Can only be True or False (the capitalization is important!).
+
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+
+savings = 100
+growth_multiplier = 1.1
+desc = "compound interest"
+
+# Assign product of growth_multiplier and savings to year1
+year1 = savings * growth_multiplier
+
+# Print the type of year1
+print (type (year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print(doubledesc)
+
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
