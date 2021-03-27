a30ea3d0901311a73f71c0c328b7ebfda8221f7e
Michael Downey <michaeldowney99@gmail.com>
2021-03-27 10:57:58 +0000

    Added Module2 Basic Calculations

---
+++ Module2_BasicCalculator.py
@@ -0,0 +1,28 @@
+# Example, do not modify!
+print(5 / 8)
+
+# Print the sum of 7 and 10
+print(7 +10)
+
+
+# Division
+print(5 / 8)
+
+# Addition
+print(7 + 10)
+
+#Exponentiation: **. This operator raises the number to its left to the power of the number to its right. For example 4**2 will give 16.
+#Modulo: %. This operator returns the remainder of the division of the number to the left by the number on its right. For example 18 % 7 equals 4.
+
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years?
+print(100*1.1**7)
