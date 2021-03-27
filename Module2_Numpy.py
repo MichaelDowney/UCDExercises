717d855fe4362e908f6532c403a3d1b0b3265042
Michael Downey <michaeldowney99@gmail.com>
2021-03-27 14:15:07 +0000

    Added Numpy  exercises from data camp

---
+++ Module2_Numpy.py
@@ -0,0 +1,81 @@
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
+
+
+# Import numpy
+import numpy as np
+height_in = [200,300,400]
+
+# Create a numpy array from height_in: np_height_in
+np_height_in = np.array(height_in)
+
+# Print out np_height_in
+print(np_height_in)
+
+# Convert np_height_in to m: np_height_m
+np_height_m = np_height_in * .0254
+
+# Print np_height_m
+print(np_height_m)
+
+# height and weight are available as regular lists
+
+# Import numpy
+import numpy as np
+
+# Create array from height_in with metric units: np_height_m
+np_height_m = np.array(height_in) * 0.0254
+
+weight_lb = [200,250,700]
+
+# Create array from weight_lb with metric units: np_weight_kg
+np_weight_kg = np.array(weight_lb) *0.453592
+
+# Calculate the BMI: bmi
+BMI = np_weight_kg/np_height_m **2
+
+# Print out bmi
+print(BMI)
+
+# height and weight are available as a regular lists
+
+# Import numpy
+import numpy as np
+
+# Calculate the BMI: bmi
+np_height_m = np.array(height_in) * 0.0254
+np_weight_kg = np.array(weight_lb) * 0.453592
+bmi = np_weight_kg / np_height_m ** 2
+
+# Create the light array
+light = bmi < 21
+
+# Print out light
+print(light)
+
+# Print out BMIs of all baseball players whose BMI is below 21
+print(bmi[light])
+
+# height and weight are available as a regular lists
+
+# Import numpy
+import numpy as np
+
+# Store weight and height lists as numpy arrays
+np_weight_lb = np.array(weight_lb)
+np_height_in = np.array(height_in)
+
+# Print out the weight at index 50
+print(np_weight_lb[2])
+
+# Print out sub-array of np_height_in: index 100 up to and including index 110
+print(np_height_in[100:111])
